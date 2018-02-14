20d4b7c141973d0a57aa0f25e029e88e7f3ee330
seok ki <wizard_i@naver.com>
2018-02-14 17:29:21 +0900

    barcode example complete

--- products/app/controllers/Products.scala
+++ products/app/controllers/Products.scala
@@ -2,7 +2,9 @@ package controllers
 
 import play.api.mvc._
 import javax.inject.Inject
-import play.api.i18n.{MessagesApi, I18nSupport}
+import play.api.i18n.{MessagesApi, I18nSupport, Messages}
+import play.api.data.Form
+import play.api.data.Forms.{mapping, longNumber, nonEmptyText}
 import services.Product
 
 class Products @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
@@ -16,4 +18,38 @@ class Products @Inject() (val messagesApi: MessagesApi) extends Controller with 
 			Ok(views.html.products.details(product))
 		}.getOrElse(NotFound)
 	}
+
+	def save = Action { implicit request =>
+		val newProductForm = productForm.bindFromRequest()
+
+		newProductForm.fold(
+			hasErrors = { form =>
+				Redirect(routes.Products.newProduct()).flashing(Flash(form.data) + ("error" -> Messages("validation.errors")))
+			},
+
+			success = { newProduct =>
+				Product.add(newProduct)
+				val message = Messages("products.new.success", newProduct.name)
+				Redirect(routes.Products.show(newProduct.ean)).flashing("success" -> message)
+			}
+		)
+	}
+
+	def newProduct = Action { implicit request =>
+		val form = 
+			if (request2flash.get("error").isDefined)
+				productForm.bind(request2flash.data)
+			else 
+				productForm
+
+		Ok(views.html.products.editProduct(form))
+	}
+
+	private val productForm: Form[Product] = Form(
+		mapping(
+			"ean" -> longNumber.verifying("validation.ean.duplicate", Product.findByEan(_).isEmpty),
+			"name" -> nonEmptyText,
+			"description" -> nonEmptyText	
+		)(Product.apply)(Product.unapply)
+	)
 }
